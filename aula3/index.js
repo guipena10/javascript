d9b0195200fd7adf942f4a4dc0dc060a7a043f66
Guilherme Pena <guilhermeapena@gmail.com>
2024-02-07 21:26:31 -0300

    organização de codigo

---
+++ aula3/index.js
@@ -0,0 +1,7 @@
+console.log('Guilherme "Pena"'); // String = Texto
+console.log("Guilherme 'Pena'"); // Number = Número 
+console.log(`'Guilherme' "Pena"`);
+
+console.log(22, 80,5, 'Guilherme Pena');
+
+console.log('Meu nome é "Guilherme". Estou aprendendo JavaScript às', 21, 'da noite')
