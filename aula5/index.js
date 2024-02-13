833f3de8fcc0ccd7d6c54f743e651a4ffee345ed
Guilherme Pena <guilhermeapena@gmail.com>
2024-02-13 19:08:07 -0300

    commit

---
+++ aula5/index.js
@@ -0,0 +1,18 @@
+let nome 
+nome = 'Gui'
+
+console.log(nome, 'nasceu em 1984')
+console.log('Em 2000', nome, 'conheceu Maria')
+console.log(nome, 'casou-se com Maria em 2012')
+console.log('Maria teve 1 filho com', nome, 'em 2015')
+console.log('O filho de', nome, 'se chama Eduardo')
+
+
+// Você consegue declarar uma variável sem inicialmente adicionar o valor
+
+let idade; // Declarou o variável
+idade = '22' // Inicializando a variável
+console.log(idade)
+idade = 23
+console.log(idade)
+
