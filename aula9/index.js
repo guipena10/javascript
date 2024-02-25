3cb825f5f27e3d4d5a9861d9b2d25d9cd41ef8c0
Guilherme Pena <guilhermeapena@gmail.com>
2024-02-25 14:46:52 -0300

    criação de code

---
+++ aula9/index.js
@@ -0,0 +1,9 @@
+const nome = 'Guilherme' // string
+const nome1 = "Guilherme" // string
+const nome2 = `Guilherme` // string
+const num1 = 10 // number
+const num2 = 10.52 // number
+let nomeAluno // undefined -> não aponta para local nenhum na memoria
+const sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memoria
+const aprovado = true // Boolean = true, false (lógico)
+
